2ca5a0a8459fb3b2fe625dffed41971d1a8903e4
Øystein S. Haaland <Oystein.S.Haaland@gmail.com>
2015-08-24 20:25:05 +0200

    Introduce helper constructor for EveSqlalchemy

--- eve_sqlalchemy/__init__.py
+++ eve_sqlalchemy/__init__.py
@@ -14,6 +14,8 @@ import flask.ext.sqlalchemy as flask_sqlalchemy
 from flask import abort
 from copy import copy
 
+from eve import Eve
+
 from eve.io.base import ConnectionException
 from eve.io.base import DataLayer
 from eve.utils import config, debug_error_message, str_to_date
@@ -339,3 +341,18 @@ class SQL(DataLayer):
                     'Unable to parse `projection` clause'
                 ))
         return client_projection
+
+
+class EveSqlalchemy(Eve):
+    """
+    EveSqlalchemy
+
+    Code that is typically common for eve-sqlalchemy APIs and/or code that should be upstreamed to
+    eve.
+    """
+    def __init__(self, auth, **kwargs):
+        """
+        We want that the auth object should have access to eve, just like objects of `media` and
+        `data` keywords.
+        """
+        super(EveSqlalchemy, self).__init__(auth=auth(self), **kwargs)
