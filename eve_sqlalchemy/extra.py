f56ef671082fb0ef4ed2cdc9b4f50284091b20e4
Øystein S. Haaland <Oystein.S.Haaland@gmail.com>
2015-11-09 22:56:56 +0100

    Add EveSqlalchemyBase in extra module

--- eve_sqlalchemy/extra.py
+++ eve_sqlalchemy/extra.py
@@ -1,7 +1,25 @@
 # -*- coding:utf-8 -*-
 
+import hashlib
+
 from eve import Eve
 
+from sqlalchemy import func
+
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+    Integer
+)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+def get_etag():
+    return hashlib.sha1().hexdigest()
 
 class EveSqlalchemy(Eve):
     """
@@ -16,3 +34,14 @@ class EveSqlalchemy(Eve):
         `data` keywords.
         """
         super(EveSqlalchemy, self).__init__(auth=auth(self), **kwargs)
+
+
+class EveSqlalchemyBase(Base):
+    """
+    EveSqlalchemyBase
+    """
+    __abstract__ = True
+    _created = Column(DateTime, default=func.now())
+    _updated = Column(DateTime, default=func.now(), onupdate=func.now())
+    _etag = Column(String(40), default=get_etag())
+    id = Column(Integer, primary_key=True, autoincrement=True)
